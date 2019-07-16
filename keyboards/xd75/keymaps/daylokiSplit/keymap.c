8da1d5d50480ddfb45b16e7da4c61fb316f2aca0
ngonzales <nathan@storylabinteractive.com>
2019-07-16 13:04:38 -0600

    Removed F1 and replaced with esc

--- keyboards/xd75/keymaps/daylokiSplit/keymap.c
+++ keyboards/xd75/keymaps/daylokiSplit/keymap.c
@@ -103,7 +103,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QW] = { /* QWERTY */
-  { KC_F1,  KC_F2,    KC_F3,            KC_F4,   KC_F5,  KC_F6,    _______, _______, _______,   KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,   },
+  { KC_ESC,  KC_F2,    KC_F3,            KC_F4,   KC_F5,  KC_F6,    _______, _______, _______,   KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,   },
   { KC_ESC,  KC_Q,    KC_W,             KC_E,    KC_R,    KC_T,   RGB_RMOD, RGB_TOG, RGB_MOD,    KC_Y,      KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,   },
   { KC_TAB,  KC_A,    LT(_NUM,KC_S),    KC_D,    KC_F,    KC_G,    RGB_HUD, RGB_TOG, RGB_HUI,    KC_H,      KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,   },
   { KC_LSFT, KC_Z,    KC_X,             KC_C,    KC_V,    KC_B,    RGB_SAD, _______, RGB_SAI,    KC_N,      KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, },
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
  [_FN] = { /* FUNCTION */
-  { _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______,   },
+  {   KC_F1, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______,   },
   { _______, _______, _______, _______, _______, _______, _______, KC_INSERT, _______, _______, _______, _______, _______, _______, _______,  },
   { _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_SLCK,   KC_CAPS, KC_NLCK, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,   },
   { _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______,  },
